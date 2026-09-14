b730d83c1bc11ab8b37b6488cd34a761fc4559e5
Atakku <atakkudev@gmail.com>
2026-09-14 19:54:04 +0200

    big updat

--- java/server/zone/hrt/worldgen/mixin/MixinNoiseBasedChunkGenerator.java
+++ java/server/zone/hrt/worldgen/mixin/MixinNoiseBasedChunkGenerator.java
@@ -1,19 +1,14 @@
 package zone.hrt.worldgen.mixin;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.WorldGenRegion;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.StructureManager;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.BiomeManager;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.NoiseBasedChunkGenerator;
-import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
 import net.minecraft.world.level.levelgen.RandomState;
 import net.minecraft.world.level.levelgen.WorldGenerationContext;
 import net.minecraft.world.level.levelgen.blending.Blender;
