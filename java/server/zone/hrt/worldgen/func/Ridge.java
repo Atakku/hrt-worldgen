f9b48e5cdb00faac752573b481c57d44ba0fb9dd
Atakku <atakkudev@gmail.com>
2026-09-20 20:27:20 +0200

    meow

--- java/server/zone/hrt/worldgen/func/Ridge.java
+++ java/server/zone/hrt/worldgen/func/Ridge.java
@@ -25,7 +25,7 @@ public record Ridge(DensityFunction temperature, DensityFunction vegetation) imp
   private static final CubicSpline<Float, ToFloatFunction<Float>> VEG_SPLINE = buildSpline(
       new float[] { -0.5f, -0.35f, -0.1f, 0.1f, 0.3f, 0.5f });
 
-  private static final float RADIUS = 0.005f;
+  private static final float RADIUS = 0.0075f;
 
   private static final CubicSpline<Float, ToFloatFunction<Float>> buildSpline(float[] input) {
     CubicSpline.Builder<Float, ToFloatFunction<Float>> spline = CubicSpline.builder(ToFloatFunction.IDENTITY);
@@ -44,10 +44,15 @@ public record Ridge(DensityFunction temperature, DensityFunction vegetation) imp
       float start = i > 0 ? Mth.lerp(0.66f, points[i - 1], point): point - (points[i + 1] - point);
       float end = i < points.length - 1 ? Mth.lerp(0.33f, point, points[i + 1]) : point + (point - points[i - 1]);
 
-      spline = spline.addPoint(start, sign * -0.75f, 0.5f);
-      spline = spline.addPoint(point - RADIUS, sign * -0.05f, 0.5f);
-      spline = spline.addPoint(point + RADIUS, sign * 0.05f, 0.5f);
-      spline = spline.addPoint(end, sign * 0.75f, 0.5f);
+      spline = spline.addPoint(start, sign * -0.75f, 0f);
+      spline = spline.addPoint(point - RADIUS * 1.25f, sign * -0.08f, 0f);
+      spline = spline.addPoint(point - RADIUS, sign * -0.08f, 0f);
+      //spline = spline.addPoint(point - RADIUS * 0.5f, sign * -0.035f, 0f);
+      //spline = spline.addPoint(point, 0, 0f);
+      //spline = spline.addPoint(point + RADIUS * 0.5f, sign * 0.035f, 0f);
+      spline = spline.addPoint(point + RADIUS, sign * 0.08f, 0f);
+      spline = spline.addPoint(point + RADIUS * 1.25f, sign * 0.08f, 0f);
+      spline = spline.addPoint(end, sign * 0.75f, 0f);
     }
     return spline.build();
   }
@@ -59,10 +64,9 @@ public record Ridge(DensityFunction temperature, DensityFunction vegetation) imp
     if (x >= Worldgen.R_BLOCKS || z >= Worldgen.R_BLOCKS || x < -Worldgen.R_BLOCKS || z < -Worldgen.R_BLOCKS)
       return 0;
 
-    float tem = (float) this.temperature.compute(pos);
-    float veg = (float) this.vegetation.compute(pos);
-
-    return TEM_SPLINE.apply(tem) * VEG_SPLINE.apply(veg);
+    float tem = TEM_SPLINE.apply((float) this.temperature.compute(pos));
+    float veg = VEG_SPLINE.apply((float) this.vegetation.compute(pos));
+    return Math.copySign(Math.min(Math.abs(tem), Math.abs(veg)), tem * veg);
   }
 
   @Override
